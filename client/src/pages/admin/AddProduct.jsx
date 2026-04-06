6bb806e462d54bfae9c34cd9fe8336cefdaf6088
abdulnoor <abdulnoorwork@gmail.com>
2026-04-06 13:13:48 +0500

    saved changes

--- client/src/pages/admin/AddProduct.jsx
+++ client/src/pages/admin/AddProduct.jsx
@@ -16,7 +16,7 @@ const AddProduct = () => {
   const editorRef2 = useRef(null);
   const quillRef2 = useRef(null)
   const navigate = useNavigate()
-  const { backendUrl, isAdmin,fetchProducts } = useContext(AppContext);
+  const { backendUrl, isAdmin, fetchProducts } = useContext(AppContext);
 
   const [loading, setLoading] = useState(false);
   const [name, setName] = useState('');
@@ -147,8 +147,8 @@ const AddProduct = () => {
                 <button className="sm:block hidden mt-2 px-4 py-2 bg-white text-blue-600 rounded-lg sm:text-xs text-[11px] font-medium cursor-pointer border border-[#E2E8F0]">
                   Upload Images
                 </button>
-              </div> : <div className='flex items-center gap-2'>{previewImage.map((img, index) => (
-                <img key={index} src={img} className='rounded cursor-pointer max-h-26 max-w-24 bg-gray-100' alt="" />
+              </div> : <div className='grid grid-cols-3 items-center gap-2'>{previewImage.map((img, index) => (
+                <figure><img key={index} src={img} className='rounded cursor-pointer w-full h-full max-h-[70px] bg-gray-100 object-cover' alt="" /></figure>
               ))}</div>}
               <input type="file" ref={file} multiple onChange={imagesHandler} hidden id='image' />
             </div>
@@ -193,7 +193,7 @@ const AddProduct = () => {
                   <div className='flex flex-col w-full'>
                     <label className="block sm:text-sm text-xs font-medium mb-1">SubCategory</label>
                     <select defaultValue={0} onChange={(e) => setSubCategory(e.target.value)} name="category" className='w-full border border-gray-300 rounded-lg px-3 py-2.5 focus:ring-2 focus:ring-blue-500 outline-none text-[13px] text-grasm:y-600'>
-    text-xs                   <option disabled value={0}>--Select SubCategory--</option>
+                      text-xs                   <option disabled value={0}>--Select SubCategory--</option>
                       <option id='Men' value="Men">Men</option>
                       <option id="Women" value="Women">Women</option>
                       <option id="Kids" value="Kids">Kids</option>
@@ -240,7 +240,7 @@ const AddProduct = () => {
                               SubCategory
                             </label>
                             <select defaultValue={0} onChange={(e) => setSubCategory(e.target.value)} name="category" className='w-full border border-gray-300 rounded-lg px-3 py-2.5 focus:ring-2 focus:ring-blue-500 outline-none text-[13px] text-gray-6sm:00'>
-       text-xs                        <option disabled value={0}>--Select SubCategory--</option>
+                              text-xs                        <option disabled value={0}>--Select SubCategory--</option>
                               <option id='Mobile & Computers' value="Mobile & Computers">Mobile & Computers</option>
                               <option id="Smart Watch & Smart TV" value="Smart Watch & Smart TV">Watch & TV</option>
                               <option id="Audio & Accessories" value="Audio & Accessories">Audio & Accessories</option>
