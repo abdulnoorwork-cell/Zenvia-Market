8613060d67fc0339a845116139054de29e3d5738
abdulnoor <abdulnoorwork@gmail.com>
2026-04-15 14:52:00 +0500

    saved

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,19 @@
 import mysql from 'mysql';
 import 'dotenv/config'
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABAE,
-    port: process.env.DB_PORT
-});
+let pool;
 
-db.connect((err)=>{
-    if(err)return console.log(err)
+try {
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABAE,
+        port: process.env.DB_PORT
+    });
     console.log("Connected to Database: " + process.env.DB_DATABAE)
-})
+} catch (error) {
+    console.log(error)
+}
 
-export default db;
+export default pool;
